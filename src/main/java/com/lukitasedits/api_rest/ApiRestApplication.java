262b4534bf646dd8352c5e2482671cf25a053bf1
Jennkerbel <83468234+Lukitasedits@users.noreply.github.com>
2025-01-25 19:38:29 -0300

    Update ApiRestApplication.java

--- src/main/java/com/lukitasedits/api_rest/ApiRestApplication.java
+++ src/main/java/com/lukitasedits/api_rest/ApiRestApplication.java
@@ -3,7 +3,6 @@ package com.lukitasedits.api_rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true)
